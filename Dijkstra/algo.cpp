58d7d4ee0938553f3e79025774884f7ed037a5c7
Nikita <nikita_kamzeev@mail.ru>
2020-10-03 21:48:07 +0300

    Add dijkstra algorithm

---
+++ Dijkstra/algo.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <fstream>
+
+// O(m*log(n))
+unsigned int dijkstra(unsigned int st, unsigned int end, const std::vector<std::vector<std::pair<unsigned int, unsigned int>>> &graph){
+    std::set<std::pair<unsigned int, unsigned int>> cands {std::make_pair(0, st)};
+    std::vector<unsigned int> ds(graph.size(), UINT32_MAX);
+    ds[st] = 0;
+    while (cands.size()){
+        auto it = cands.begin();
+        unsigned int v = (*it).second;
+        for(auto &p: graph[v]){
+            if(ds[v] + p.first < ds[p.second]){
+                cands.erase(std::make_pair(ds[p.second], p.second));  // to update
+                ds[p.second] = ds[v] + p.first;
+                cands.insert(std::make_pair(ds[p.second], p.second));
+            }
+        }
+        cands.erase(it);
+    }
+    return ds[end];   
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    size_t m, n;
+    std::ifstream input("input.txt");
+    input >> n >> m;
+    std::vector<std::vector<std::pair<unsigned int, unsigned int>>> graph(n);
+    unsigned int u, v, w;
+    for(int i=0;i < m; i++){
+        input >> u >> v >> w;
+        graph[u-1].push_back(std::make_pair(w, v-1));
+        graph[v-1].push_back(std::make_pair(w, u-1));
+    }
+    input.close();
+
+    unsigned int min_cost = dijkstra(0, n-1, graph);
+    std::ofstream output("output.txt");
+    output << min_cost;
+    output.close();
+
+    return 0;
+}
---
+++ Dijkstra/algo.py
@@ -0,0 +1,29 @@
+import heapq as pq
+
+## O(m*log(m))
+def dijkstra(st, end, graph):
+    heap = [(0, st)]
+    ds = [float('inf')]*len(graph)
+    ds[st] = 0
+    used = set()
+    while heap:
+        d, v = pq.heappop(heap)
+        if d <= ds[v] and v not in used: 
+            used.add(v)
+            for to_len, to in graph[v]:
+                if ds[v] + to_len < ds[to]:
+                    ds[to] = ds[v] + to_len
+                    pq.heappush(heap, (ds[to], to))
+    return ds[end]
+
+
+if __name__ == "__main__":    
+    with open('input.txt') as f:
+        n, m = map(int, f.readline().split())
+        graph = [[] for i in range(n)]
+        for line in f:
+            u, v, w = map(int, line.split())
+            graph[u-1].append((w, v-1))
+            graph[v-1].append((w, u-1))
+    with open('output.txt', 'w') as f:
+        f.write(str(dijkstra(0, n-1, graph)))
